3c5419acfd73d642ee0ec22434674f7e9c2423e9
Claudius <claudius@Claudiuss-MacBook-Pro.local>
2025-11-02 12:47:30 +0800

    add_setting_page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,13 @@ import '@/global.css';
 import { NAV_THEME } from '@/lib/theme';
 import { ThemeProvider } from '@react-navigation/native';
 import { PortalHost } from '@rn-primitives/portal';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
+import { Image } from 'react-native-svg';
+import Feather from '@expo/vector-icons/Feather';
+import Entypo from '@expo/vector-icons/Entypo';
+import { StackScreen } from 'react-native-screens';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -14,11 +18,37 @@ export {
 
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
+  const router = useRouter();
 
   return (
     <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack />
+      <Stack>
+        <Stack.Screen
+          name="settings/index"
+          options={{
+            headerShown: true,
+            headerTitle: 'wow this is stupid',
+            headerBackVisible: false,
+            headerRight: () => <Feather name="settings" size={24} color="black" />,
+            headerLeft: () => (
+              <Entypo
+                onPress={() => router.push('/')}
+                name="chevron-with-circle-left"
+                size={24}
+                color="black"
+              />
+            ),
+            // headerBackImageSource: () => (<Image></Image>)
+          }}
+        />
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
       <PortalHost />
     </ThemeProvider>
   );
