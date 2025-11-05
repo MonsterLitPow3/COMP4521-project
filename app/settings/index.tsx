31b30c46abe0db173c3f77bab177da2aa6c42a0d
LAM, Yuen Hei <yhlamaw@connect.ust.hk>
2025-11-05 20:27:40 +0800

    Revert "Feature/dashboard"

--- app/settings/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function Settings() {
-  return (
-    <View className="items-center justify-center bg-green-300">
-      <Text>test</Text>
-    </View>
-  );
-}
